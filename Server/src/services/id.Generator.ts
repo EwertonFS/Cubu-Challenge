b01f4c6eba7ea5d89cc2314c1324d5285d16ccbd
ewerton <ewerton.lost2@gmail.com>
2023-01-14 23:16:07 -0300

    update

--- Server/src/services/id.Generator.ts
+++ Server/src/services/id.Generator.ts
@@ -1,5 +1,5 @@
-/* export class IdGeneretor {
+/*  export class IdGeneretor {
     //to string porque colocamos na função que seria uma string
     execute =() => Date.now()
     //.toString() retirei esse trecho para tentar funcionar
-} */
+}  */
